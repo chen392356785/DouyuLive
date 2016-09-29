c9ea6bb144f00379c2925af5debe70cbae347f39
chenxiaolei <chenxiaolei0419@163.com>
2016-09-29 11:16:15 +0800

    初始化项目

---
+++ DouyuLive/DouyuLive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DouyuLive
+//
+//  Created by chenxiaolei on 2016/9/29.
+//  Copyright © 2016年 chenxiaolei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
